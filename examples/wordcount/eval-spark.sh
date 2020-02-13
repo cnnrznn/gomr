83166f43bb2f561bef29cbc638ca66ed445815b9
Connor Zanin <cnnrznn@gmail.com>
2020-02-13 15:39:03 -0500

    turn off spark logs in output timing file

--- examples/wordcount/eval-spark.sh
+++ examples/wordcount/eval-spark.sh
@@ -4,6 +4,6 @@ for par in 1 2 4 8
 do
     for trial in $(seq 10)
     do
-    /usr/bin/time -f "${par} %e" spark-submit spark.py moby.txt $par >out.spark.txt
+    /usr/bin/time -f "${par} %e" sh -c "spark-submit spark.py moby.txt $par >out.spark.txt 2>/dev/null"
     done
 done 2>>times.spark.txt
