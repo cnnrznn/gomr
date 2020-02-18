9c5ced3d636033ef473db52e76ec27e608f17083
Connor Zanin <cnnrznn@gmail.com>
2020-02-18 16:21:49 -0500

    moved all data input code to different source file

--- gomr.go
+++ gomr.go
@@ -1,11 +1,7 @@
 package gomr
 
 import (
-	"bufio"
-	"io"
 	"log"
-	"math"
-	"os"
 	"sync"
 )
 
@@ -24,112 +20,6 @@ type Partitioner interface {
 
 type Reducer interface {
 	Reduce(in <-chan interface{}, out chan<- interface{}, wg *sync.WaitGroup)
-}
-
-func TextFileSerial(fn string, inMap chan interface{}) {
-	file, _ := os.Open(fn)
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		inMap <- scanner.Text()
-	}
-
-	close(inMap)
-}
-
-func TextFileMultiplex(fn string, inMap []chan interface{}) {
-	par := len(inMap)
-
-	file, _ := os.Open(fn)
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	for d := 0; scanner.Scan(); d = (d + 1) % par {
-		inMap[d] <- scanner.Text()
-	}
-
-	for _, ch := range inMap {
-		close(ch)
-	}
-}
-
-func TextFileParallel(fn string, inMap []chan interface{}) {
-	file, err := os.Open(fn)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	stat, err := file.Stat()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	size := stat.Size()
-	nChunks := len(inMap)
-	chunkSize := int64(math.Ceil(float64(size) / float64(nChunks)))
-
-	for i := 0; i < nChunks; i++ {
-		go func(i int) {
-			buffer := make([]byte, FILEBUF)
-			atEOF := false
-			skippedFirst := false
-
-			start := chunkSize * int64(i)
-			end := start + chunkSize
-			bufstart, bufend := 0, 0
-			log.Println(i, start, end)
-
-			file, _ := os.Open(fn)
-			defer file.Close()
-
-			pos, err := file.Seek(start, 0)
-			if err != nil || pos != start {
-				log.Fatal(pos, err)
-			}
-
-			for start <= end && !atEOF {
-				copy(buffer, buffer[bufstart:bufend])
-				bufend -= bufstart
-
-				n, err := file.Read(buffer[bufend:])
-				if err != nil {
-					if err == io.EOF {
-						atEOF = true
-					} else {
-						log.Fatal(err)
-					}
-				}
-
-				bufstart = 0
-				bufend += n
-
-				for start <= end {
-					advance, token, err := bufio.ScanLines(buffer[bufstart:bufend], atEOF)
-					if err != nil {
-						log.Fatal(err) // ScanLines doesn't throw an error ever in the source code
-					}
-
-					if advance == 0 {
-						break
-					}
-
-					bufstart += advance
-					start += int64(advance)
-
-					if i == 0 || skippedFirst {
-						inMap[i] <- string(token)
-					}
-					skippedFirst = true
-				}
-			}
-
-			close(inMap[i])
-		}(i)
-	}
 }
 
 /*
