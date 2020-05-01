ac3d4c3dd4bf9265a3c9d4130b0d38d7a2c6f7d2
Connor Zanin <cnnrznn@gmail.com>
2020-05-01 12:57:29 -0400

    a utility for chaining mapreduce jobs

---
+++ chain.go
@@ -0,0 +1,19 @@
+package gomr
+
+/*
+Chain two Jobs together by feeding the output from one
+Job to the inputs of another.
+*/
+func Chain(in <-chan interface{}, outs []chan interface{}) {
+	go func() {
+		i := 0
+		for e := range in {
+			outs[i] <- e
+			i = (i + 1) % len(outs)
+		}
+
+		for _, ch := range outs {
+			close(ch)
+		}
+	}()
+}
