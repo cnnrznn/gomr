156b0d99eec1656a82218d96f7804d74ddf23160
Connor Zanin <cnnrznn@gmail.com>
2023-07-20 16:08:53 -0600

    remove sync dependency

--- interface.go
+++ interface.go
@@ -2,7 +2,6 @@ package gomr
 
 import (
 	"fmt"
-	"sync"
 )
 
 type Mapper interface {
@@ -10,7 +9,7 @@ type Mapper interface {
 }
 
 type Reducer interface {
-	Reduce(in <-chan any, out chan<- any, wg *sync.WaitGroup)
+	Reduce(in <-chan any, out chan<- any)
 }
 
 type Processor interface {
