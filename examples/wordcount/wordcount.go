8db27e1744c55950577c587a50dbfb6141997a79
Connor Zanin <cnnrznn@gmail.com>
2023-07-24 17:47:56 -0600

    Wordcount example program under new arch.
    Use this as a blueprint for implementing tier2 - "test driven design"

---
+++ examples/wordcount/wordcount.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/cnnrznn/gomr"
+)
+
+type Data struct {
+	Word  string `json:"word"`
+	Count int    `json:"count"`
+}
+
+func (d Data) Key() string {
+	return d.Word
+}
+
+type WordcountProc struct{}
+
+func (w *WordcountProc) Map(in <-chan []byte, out chan<- gomr.Keyer) error {
+	defer close(out)
+
+	counts := make(map[string]int)
+
+	for row := range in {
+		words := strings.Split(string(row), " ")
+		for _, word := range words {
+			counts[word]++
+		}
+	}
+
+	for k, v := range counts {
+		out <- Data{
+			Word:  k,
+			Count: v,
+		}
+	}
+
+	return nil
+}
+
+func (w *WordcountProc) Reduce(in <-chan []byte, out chan<- any) error {
+	defer close(out)
+
+	count := 0
+	word := ""
+
+	for row := range in {
+		data := Data{}
+		err := json.Unmarshal(row, &data)
+		if err != nil {
+			return err
+		}
+
+		count++
+		word = data.Word
+	}
+
+	out <- Data{Word: word, Count: count}
+
+	return nil
+}
+
+func main() {
+	fmt.Println("Starting wordcount")
+
+	job := gomr.Job{}
+}
