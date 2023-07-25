5e3d9da87f03472bc38830e11058315332b8d708
Connor Zanin <cnnrznn@gmail.com>
2023-07-25 12:04:21 -0600

    Cleaning up the wordcount example

--- examples/wordcount/wordcount.go
+++ examples/wordcount/wordcount.go
@@ -6,7 +6,18 @@ import (
 	"strings"
 
 	"github.com/cnnrznn/gomr"
+	"github.com/cnnrznn/gomr/store"
 )
+
+type Line struct {
+	Payload string
+}
+
+func (l Line) Key() string                { return "" }
+func (l Line) Serialize() ([]byte, error) { return nil, nil }
+func (l Line) Deserialize(bs []byte) (gomr.Data, error) {
+	return Line{Payload: string(bs)}, nil
+}
 
 type Data struct {
 	Word  string `json:"word"`
@@ -22,15 +33,21 @@ func (d Data) Serialize() ([]byte, error) {
 	return bs, err
 }
 
+func (d Data) Deserialize(bs []byte) (gomr.Data, error) {
+	data := Data{}
+	err := json.Unmarshal(bs, &data)
+	return data, err
+}
+
 type WordcountProc struct{}
 
-func (w *WordcountProc) Map(in <-chan []byte, out chan<- gomr.Keyer) error {
+func (w *WordcountProc) Map(in <-chan gomr.Data, out chan<- gomr.Data) error {
 	defer close(out)
 
 	counts := make(map[string]int)
 
 	for row := range in {
-		words := strings.Split(string(row), " ")
+		words := strings.Split(row.(Line).Payload, " ")
 		for _, word := range words {
 			counts[word]++
 		}
@@ -46,20 +63,19 @@ func (w *WordcountProc) Map(in <-chan []byte, out chan<- gomr.Keyer) error {
 	return nil
 }
 
-func (w *WordcountProc) Reduce(in <-chan []byte, out chan<- any) error {
+func (w *WordcountProc) Reduce(in <-chan gomr.Data, out chan<- gomr.Data) error {
 	defer close(out)
 
 	count := 0
 	word := ""
 
 	for row := range in {
-		data := Data{}
-		err := json.Unmarshal(row, &data)
-		if err != nil {
-			return err
+		data := row.(Data)
+		if word != "" && word != data.Word {
+			return fmt.Errorf("Reducer supplied multiple keys")
 		}
 
-		count++
+		count += data.Count
 		word = data.Word
 	}
 
@@ -71,10 +87,32 @@ func (w *WordcountProc) Reduce(in <-chan []byte, out chan<- any) error {
 func main() {
 	fmt.Println("Starting wordcount")
 
+	inputStore := store.MemStore{
+		Data: [][]byte{
+			[]byte(" these are words\n"),
+			[]byte("written on a page"),
+			[]byte("that no one will read"),
+		},
+	}
+	inputStore.Init(store.Config{
+		Name: "in1",
+		Node: "localhost:8080",
+	})
+
 	job := gomr.Job{
 		Proc: &WordcountProc{},
 		Name: "wordcount",
+
+		InType:   Line{},
+		InStores: []store.Store{&inputStore},
+
+		Cluster: gomr.Cluster{
+			Nodes: []string{
+				"localhost:8080",
+			},
+			Self: 0,
+		},
 	}
 
-	gomr.
+	fmt.Println(job)
 }
