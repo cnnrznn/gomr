f41a0f1a21c99646b87d73ddc3d7f9689f138e25
Connor Zanin <cnnrznn@gmail.com>
2023-09-18 10:49:40 -0600

    Put directory creation and cleanup in tier 3

--- tier3.go
+++ tier3.go
@@ -1,12 +1,34 @@
 package gomr
 
+import "os"
+
 func (j *Job) MapReduce() error {
+	setup()
+	defer destroy()
+
 	err := j.doMap()
 	if err != nil {
 		return err
 	}
+
 	j.doShuffle()
+
 	j.doReduce()
 
 	return nil
+}
+
+const (
+	DIR_PRESHUFFLE  = "preshuffle"
+	DIR_POSTSHUFFLE = "postshuffle"
+)
+
+func setup() {
+	os.Mkdir(DIR_PRESHUFFLE, 0755)
+	os.Mkdir(DIR_POSTSHUFFLE, 0755)
+}
+
+func destroy() {
+	os.RemoveAll(DIR_PRESHUFFLE)
+	os.RemoveAll(DIR_POSTSHUFFLE)
 }
